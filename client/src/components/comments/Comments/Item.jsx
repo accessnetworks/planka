9ca5774c340b6c07058a09357dccc3a66697aab3
lustsazeus-lab <lustsazeus@gmail.com>
2026-03-06 09:01:39 +0800

    feat: add comment reply shortcut and description mentions
    (cherry picked from commit f23ce6e025c8b5c3a6dbcdb21c6db172ef3202f1)

--- client/src/components/comments/Comments/Item.jsx
+++ client/src/components/comments/Comments/Item.jsx
@@ -25,7 +25,7 @@ import UserAvatar from '../../users/UserAvatar';
 
 import styles from './Item.module.scss';
 
-const Item = React.memo(({ id }) => {
+const Item = React.memo(({ id, canReply, onReply }) => {
   const selectCommentById = useMemo(() => selectors.makeSelectCommentById(), []);
   const selectUserById = useMemo(() => selectors.makeSelectUserById(), []);
   const selectListById = useMemo(() => selectors.makeSelectListById(), []);
@@ -92,6 +92,12 @@ const Item = React.memo(({ id }) => {
     setIsEditOpened(true);
   }, []);
 
+  const handleReplyClick = useCallback(() => {
+    if (user.username) {
+      onReply(user.username);
+    }
+  }, [onReply, user.username]);
+
   const handleEditClose = useCallback(() => {
     setIsEditOpened(false);
   }, []);
@@ -126,8 +132,18 @@ const Item = React.memo(({ id }) => {
               <span className={styles.date}>
                 <TimeAgo date={comment.createdAt} />
               </span>
-              {(canEdit || canDelete) && (
+              {(canReply || canEdit || canDelete) && (
                 <span className={styles.actions}>
+                  {canReply && user.username && (
+                    <Comment.Action
+                      as="button"
+                      content={t('action.reply', {
+                        defaultValue: 'Reply',
+                      })}
+                      disabled={!comment.isPersisted}
+                      onClick={handleReplyClick}
+                    />
+                  )}
                   {canEdit && (
                     <Comment.Action
                       as="button"
@@ -162,6 +178,13 @@ const Item = React.memo(({ id }) => {
 
 Item.propTypes = {
   id: PropTypes.string.isRequired,
+  canReply: PropTypes.bool,
+  onReply: PropTypes.func,
+};
+
+Item.defaultProps = {
+  canReply: false,
+  onReply: () => {},
 };
 
 export default Item;
